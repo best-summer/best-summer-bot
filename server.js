e94ddf1c177e86d989c6292239a304355306236f
garicchi <xgaricchi@gmail.com>
2017-06-25 22:16:44 +0900

    fix

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var request = require('request');
 const app = express();
 
 var webhookUrl = 'https://hooks.slack.com/services/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';
+var registerdToken = 'VNRER3lMlUPzRMeuaerBgrfP';
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -12,6 +13,14 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 1337);
+
+function sendResponse(res,text){
+    var payload = {
+      "text": response,
+      "username":'best-summer'
+    };
+    res.send(200, JSON.stringify(payload));
+}
 
 app.post('/', function (req, res) {
   var body = req.body;
@@ -21,19 +30,19 @@ app.post('/', function (req, res) {
   var trigger_word = body['trigger_word'];
   var response = '';
 
-  if(text.indexOf('@best-summer') != -1){
-    response = text.replace('@best-summer','');
+  if(token != registerdToken){
+      return;
   }
-  
-  var payload = {
-      "text": response,
-      "username":'best-summer'
-  };
 
-/*
-  request.post(payload, function (error, response, body) {
-    console.log(JSON.stringify(response));
-  });
-  */
-  res.send(200, JSON.stringify(payload));
-})
+  if(text.indexOf('@best-summer') != -1){
+    message = text.replace('@best-summer','');
+    sendResponse(res,'@'+user_name+' エモ');
+  }else{
+    var rand = Math.floor(Math.random()*5);
+    if(rand == 0){
+        sendResponse(res,'エモい～');
+    }else if(rand == 1){
+        sendResponse(res,'ソーシャル！ソーシャル！');
+    }
+  }
+});
